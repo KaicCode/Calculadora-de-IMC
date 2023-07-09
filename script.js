634b15e0832a8b1b8abaa8bbfe722dfded8fec78
Kaic <136115920+KaicCode@users.noreply.github.com>
2023-07-09 13:05:19 -0300

    IMC

---
+++ script.js
@@ -0,0 +1,14 @@
+var peso = 0;
+var altura = 0;
+
+function limpar(){
+    console.log("Esta limpando");
+}
+
+function calcular(){
+   document.getElementById('altura').value;
+   if () {
+    
+   }
+   document.getElementById('story').innerText = 'teste'
+}
