828c38993a8355d1ceff22d3debc8b6139d8e492
Kaic <136115920+KaicCode@users.noreply.github.com>
2023-07-09 17:34:58 -0300

    IMC

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-var peso = 0;
-var altura = 0;
 
 function limpar(){
     console.log("Esta limpando");
 }
 
 function calcular(){
-   document.getElementById('altura').value;
-   if () {
+    var altura = document.getElementById("altura").value
+    var peso = document.getElementById("peso").value
     
-   }
-   document.getElementById('story').innerText = 'teste'
+    var imc = peso / altura **2
+    console.log(imc)
+    var text = ""
+    if (imc<18.5) {
+       text = ("Você está magro")
+    }else if(imc<24.9){
+        text = ("Você está normal")
+    }else if(imc<29.9){
+        text = ("Você está acima do peso")
+    }else if(imc<39.9){
+        text = ("Você está com obesidade")
+    }else if(imc>39.9){
+        text = ("Você está com obesidade mórbida")
+    }
+    document.getElementById("story").innerText = text
 }
